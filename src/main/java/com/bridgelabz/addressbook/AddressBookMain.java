11a2b4d23108cc09b72ce916c3d8ac2633213d18
Prathamesh Hadalgekar <hadalgekarprathamesh@gmail.com>
2022-12-19 12:09:45 +0530

    adding program to create contacts

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("---------- Welcome To Address Book Program!! ----------");
+	}
+
+}
+
+
