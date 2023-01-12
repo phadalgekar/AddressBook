cec2cebbdc8181581fbd5b0dd278ee9f4cd72a0b
Prathamesh Hadalgekar <hadalgekarprathamesh@gmail.com>
2023-01-12 12:42:44 +0530

    adding program to restrict duplicate entry in address book using collection

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+	public static Scanner scannerObject = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("---------- Welcome To Address Book Program!! ----------");
 		
@@ -9,11 +12,11 @@ public class AddressBookMain {
         AddressBook addressBook = new AddressBook();
         addressBook.operation();
 	}
-
-
-
+	
 }
 	
+
+		
 	
 
 
